fe7b46531ca8aeeadf6c8af2f0e19a91af4468b3
JIELSAN <jesussjg811@gmail.com>
2024-10-07 18:06:32 -0600

    endpoints

--- routes/alumnosRoutes.js
+++ routes/alumnosRoutes.js
@@ -5,11 +5,41 @@ const express = require('express');
 const rutas = express.Router();
 
 //definir un endpoinst para obtener la lista de los alumnos
+
+
 //METODO:GET(leer)
 rutas.get('/', function (req, res) {
-    res.send("<h1>hola mundo</h1>")
+    //res.send("<h1>hola mundo</h1>")
     res.json({
         message: "hola mundo"
+    })
+})
+
+//METODO: POST(crear)
+rutas.post('/fichas', function (req, res) {//servicio
+    res.json({
+        message: "ficha creada con exito"
+    })
+})
+
+//METODO: POST(crear)
+rutas.post('/becas', function (req, res) {//servicio
+    res.json({
+        message: "Registro de beca creada"
+    })
+})
+
+//METODO: GET(leer)
+rutas.get('/calificaciones/:matricula', function (req, res) {//servicio
+    res.json({
+        message: "Lista de calificaciones"
+    })
+})
+
+//METODO: POST(leer)
+rutas.post('/inscripciones', function (req, res) {
+    res.json({
+        message: "Inscripcion creada"
     })
 })
 
