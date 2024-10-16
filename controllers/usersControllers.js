7c1c808150679ac63657627b022c654576e6fdd8
JIELSAN <jesussjg811@gmail.com>
2024-10-16 16:06:27 -0600

    verificacion de campos de db

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,22 +1,31 @@
 const connection = require('../models/database');
+const db = require('../models/database');
 
 exports.login = (req, res) => {
-    const {username, password} = req.body
-    console.log(username)
-    console.log(password)
-    /*res.json({
-        success:true
-    })*/
+    const { username, password } = req.body
+
+    const query = 'SELECT * FROM `usuarios` WHERE username =? AND password =?;'
+
+    //Verificar las credenciales de un usuario y responde en consecuencia.
+    db.query(query, [username, password], (err, results) => {
+        if (err) throw err;
+        if (results.length > 0) {
+            res.json({ message: "Login Exitoso", data: results })
+            //Generar un token
+        } else {
+            res.json({ message: "Usuario o contraseña incorrectos", data: [] })
+        }
+    })
 }
 
 exports.register = (req, res) => {
     res.json({
-        success:true
+        success: true
     })
 }
 
 exports.allUsers = (req, res) => {
     res.json({
-        success:true
+        success: true
     })
 }
